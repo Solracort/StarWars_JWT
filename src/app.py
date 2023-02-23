d3ed3eb15cdebcd07532fb566f0434f24f0acc24
Solracort <solracort@gmail.com>
2023-02-23 19:38:01 +0000

    Con JWT

--- src/app.py
+++ src/app.py
@@ -8,8 +8,12 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Personaje
 #from models import Person
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -37,6 +41,18 @@ def sitemap():
     return generate_sitemap(app)
 
 #ENDPOINTS 
+@app.route('/personaje', methods=['GET'])
+def handle_personaje():
+    characters= Personaje.query.all()
+    results=list(map(lambda item:item.serialize(),characters))
+    print(results)
+    response_body = {
+        "msg": "Hello, this is your GET /personaje response:  ",
+        "results": results
+    }
+
+    return jsonify(response_body), 200
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -46,6 +62,23 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    
+    user = User.query.filter_by(email=email).first()
+    
+    if (email != user.email) or password != (user.password) :
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
